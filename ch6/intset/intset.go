7457bae3a4e8c6772ad7835e13792a6eb7aff120
Qi Wu <qiwu@alumni.stanford.edu>
2022-07-28 14:44:48 +0800

    ex6.1

--- ch6/intset/intset.go
+++ ch6/intset/intset.go
@@ -37,22 +37,55 @@ func (s *IntSet) UnionWith(t *IntSet) {
 	}
 }
 
+func (s *IntSet) Len() int {
+	return len(s.items())
+}
+
+func (s *IntSet) Remove(x int) {
+	word, bit := x/64, uint(x%64)
+	if word >= len(s.words) {
+		return
+	}
+	s.words[word] &= ^(1 << bit)
+}
+
+func (s *IntSet) Clear() {
+	s.words = []uint64{}
+}
+
+func (s *IntSet) Copy() *IntSet {
+	newWords := make([]uint64, len(s.words))
+	copy(newWords, s.words)
+	return &IntSet{
+		words: newWords,
+	}
+}
+
 func (s *IntSet) String() string {
 	var buf bytes.Buffer
 	buf.WriteByte('{')
+	items := s.items()
+	for _, i := range items {
+		if buf.Len() > len("{") {
+			buf.WriteByte(' ')
+		}
+		fmt.Fprintf(&buf, "%d", i)
+	}
+	buf.WriteByte('}')
+	return buf.String()
+}
+
+func (s *IntSet) items() []int {
+	items := []int{}
 	for i, word := range s.words {
 		if word == 0 {
 			continue
 		}
 		for j := 0; j < 64; j++ {
 			if word&(1<<j) != 0 {
-				if buf.Len() > len("{") {
-					buf.WriteByte(' ')
-				}
-				fmt.Fprintf(&buf, "%d", 64*i+j)
+				items = append(items, 64*i+j)
 			}
 		}
 	}
-	buf.WriteByte('}')
-	return buf.String()
+	return items
 }
--- ch6/intset/intset_test.go
+++ ch6/intset/intset_test.go
@@ -87,3 +87,144 @@ func TestHas(t *testing.T) {
 		})
 	}
 }
+
+func TestLen(t *testing.T) {
+	i := IntSet{}
+	tests := []struct {
+		item     int
+		expected int
+	}{
+		{
+			item:     1,
+			expected: 1,
+		},
+		{
+			item:     2,
+			expected: 2,
+		},
+		{
+			item:     1,
+			expected: 2,
+		},
+		{
+			item:     4,
+			expected: 3,
+		},
+		{
+			item:     65,
+			expected: 4,
+		},
+		{
+			item:     129,
+			expected: 5,
+		},
+	}
+
+	fmt.Println(i.String())
+	for _, test := range tests {
+		name := fmt.Sprintf("len after adding %d", test.item)
+		t.Run(name, func(t *testing.T) {
+			i.Add(test.item)
+			res := i.Len()
+			if res != test.expected {
+				t.Errorf("Has result %d, expected: %d", res, test.expected)
+			}
+		})
+	}
+}
+
+func TestRemove(t *testing.T) {
+	i := IntSet{}
+	i.Add(1)
+	i.Add(2)
+	i.Add(4)
+	i.Add(65)
+	i.Add(129)
+	tests := []struct {
+		item     int
+		expected string
+	}{
+		{
+			item:     2,
+			expected: "{1 4 65 129}",
+		},
+		{
+			item:     2,
+			expected: "{1 4 65 129}",
+		},
+		{
+			item:     2000,
+			expected: "{1 4 65 129}",
+		},
+		{
+			item:     129,
+			expected: "{1 4 65}",
+		},
+	}
+
+	for _, test := range tests {
+		name := fmt.Sprintf("remove %d", test.item)
+		t.Run(name, func(t *testing.T) {
+			i.Remove(test.item)
+			res := i.String()
+			if res != test.expected {
+				t.Errorf("Has result %s, expected: %s", res, test.expected)
+			}
+		})
+	}
+}
+
+func TestClear(t *testing.T) {
+	i := IntSet{}
+	i.Add(1)
+	i.Add(2)
+	i.Add(4)
+	i.Add(65)
+	i.Add(129)
+
+	t.Run("clear set", func(t *testing.T) {
+		i.Clear()
+		if i.Has(1) {
+			t.Error("should not include 1 since cleared")
+		}
+		if i.Len() != 0 {
+			t.Errorf("expect 0 length, got %d", i.Len())
+		}
+	})
+}
+
+func TestCopy(t *testing.T) {
+	i := IntSet{}
+	i.Add(1)
+	i.Add(2)
+	i.Add(4)
+	i.Add(65)
+	i.Add(129)
+
+	i2 := i.Copy()
+
+	t.Run("Copy", func(t *testing.T) {
+		i.Remove(1)
+		if i.Has(1) {
+			t.Error("original set should not have 1")
+		}
+		if !i2.Has(1) {
+			t.Error("copy set should still have 1")
+		}
+		i2.Add(10)
+		if i.Has(10) {
+			t.Error("original set should not have 10")
+		}
+		if !i2.Has(10) {
+			t.Error("copy set should have 10")
+		}
+		i.Add(1000)
+		if !i.Has(1000) {
+			t.Error("original set should have 1000")
+		}
+		if i2.Has(1000) {
+			t.Error("copy set should not have 1000")
+		}
+
+	})
+}
